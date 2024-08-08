28220156a73b5378d9dc282d359519df1ddf5096
Isaac Chen <isaacjchen1@gmail.com>
2024-08-07 22:27:03 -0400

    did some array stuff (it's pretty not canon)

---
+++ src/generators/the_kitchen/impl_input_generator_for_array.rs
@@ -0,0 +1,111 @@
+use crate::{
+    input_generator::{InputWithShrinkable, NextAttempt},
+    report::Observation,
+    InputGenerator,
+};
+
+/// Okay so hear me out - what if we implemented [`InputGenerator`] for arrays?
+///
+/// TODO: consider long-term API/orphan rules/specialization consequences
+
+impl<T: Clone, const N: usize> InputGenerator for [T; N] {
+    type Input = T;
+    type ShrinkableInput = usize;
+
+    type History = ();
+
+    fn cardinality(&self) -> Option<usize> {
+        assert!(!self.is_empty());
+
+        Some(N)
+    }
+
+    fn exhaustive(
+        &self,
+    ) -> impl Iterator<Item = InputWithShrinkable<Self::Input, Self::ShrinkableInput>> {
+        assert!(!self.is_empty());
+
+        self.into_iter()
+            .enumerate()
+            .map(|(index, value)| InputWithShrinkable(value.clone(), index))
+    }
+
+    fn adversarial_count(&self) -> Option<usize> {
+        assert!(!self.is_empty());
+
+        Some(0)
+    }
+
+    fn adversarial(
+        &self,
+    ) -> impl Iterator<Item = InputWithShrinkable<Self::Input, Self::ShrinkableInput>> {
+        assert!(!self.is_empty());
+
+        std::iter::empty()
+    }
+
+    fn sample(
+        &self,
+        rng: &mut (impl rand::Rng + ?Sized),
+    ) -> InputWithShrinkable<Self::Input, Self::ShrinkableInput> {
+        assert!(!self.is_empty());
+
+        let index = rng.gen_range(0..self.len());
+
+        InputWithShrinkable(self[index].clone(), index)
+    }
+
+    fn new_history(&self) -> Self::History {
+        assert!(!self.is_empty());
+
+        // TODO(ichen): implement for real
+        ()
+    }
+
+    fn update_history(
+        &self,
+        _history: &mut Self::History,
+        _shrinkable_input: Self::ShrinkableInput,
+        _test_passed: bool,
+    ) {
+        assert!(!self.is_empty());
+
+        // TODO(ichen): implement for real
+    }
+
+    fn generate_observations(&self, _history: Self::History) -> Vec<Observation> {
+        assert!(!self.is_empty());
+
+        // TODO(ichen): implement for real
+        vec![]
+    }
+
+    fn next_input(
+        &self,
+        _rng: &mut impl crate::rand::Rng,
+        _history: &Self::History,
+    ) -> NextAttempt<Self::Input, Self::ShrinkableInput> {
+        assert!(!self.is_empty());
+
+        // TODO(ichen): implement for real
+        NextAttempt::Done
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::run_test;
+
+    #[test]
+    fn test_array() {
+        let report = run_test(
+            |&n| n < 5,
+            [0, 1, 2, 3, 4, 5, 6, 7, 8],
+            &mut crate::rand::thread_rng(),
+        )
+        .unwrap_err();
+        assert_eq!(report.passing_runs, 5);
+        assert_eq!(report.shrink_steps, vec![]);
+        assert_eq!(report.simplest_failing_input, 5);
+    }
+}
--- src/generators/the_kitchen/mod.rs
+++ src/generators/the_kitchen/mod.rs
@@ -10,5 +10,6 @@
 #![allow(dead_code)]
 
 mod add_adversarial;
+mod impl_input_generator_for_array;
 mod impl_input_generator_for_slice;
 mod replace_adversarial;
