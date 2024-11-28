8657f56c712f547a3cd999f0c9cc3280fc581785
Isaac Chen <isaacjchen1@gmail.com>
2024-11-27 19:08:32 -0500

    improved panic based test runner

--- adversary/src/test_runners/panicking.rs
+++ adversary/src/test_runners/panicking.rs
@@ -1,8 +1,8 @@
 use std::{
     any::Any,
-    cell::OnceCell,
-    panic::{RefUnwindSafe, UnwindSafe},
-    sync::mpsc::TrySendError,
+    cell::{Cell, RefCell},
+    panic::{PanicHookInfo, RefUnwindSafe, UnwindSafe},
+    sync::{Arc, LazyLock, Mutex},
 };
 
 use crate::{
@@ -25,47 +25,145 @@ fn extract_panic_message(any: Box<dyn Any + Send + 'static>) -> Option<String> {
     any.downcast::<String>().ok().map(|s| *s)
 }
 
+// TODO: document how this interacts with the panic hook (no other code besides
+// other calls to this function may set the panic hook for the entire duration
+// of any call to this function, or arbitrarily weird things (but not Undefined
+// Behavior) may happen with the panic hook)
 pub fn run_test_panics<T: UnwindSafe>(
     test: impl Fn(T) + RefUnwindSafe,
     generator: impl IntoInputGenerator<T>,
     rng: &mut impl Rng,
 ) -> Result<(), Report<T>> {
-    // TODO(ichen): see if we can avoid interior mutability hacks here
-    let panic_message = OnceCell::new();
-    let (tx, rx) = std::sync::mpsc::sync_channel::<Option<PanicLocation>>(1);
+    enum PanicHookStatus {
+        NotReplaced,
+        Replaced {
+            old_hook: Box<dyn Fn(&PanicHookInfo<'_>) + 'static + Sync + Send>,
+            active_testing_threads: usize,
+        },
+    }
+    thread_local! {
+        /// Whether or not this thread is currently running an adversary panic
+        /// based test.
+        static THREAD_PANIC_TESTING: Cell<bool> = Cell::new(false);
 
-    // TODO(ichen): consider whether or not it's possible that we fail to
-    // reinstate the panic hook after a panic somewhere between now and the end
-    // of this function call... also surely this just completely trolls if
-    // another thread running happens to panic at the same time as this running
-    let old_panic_hook = std::panic::take_hook();
-    std::panic::set_hook(Box::new(move |panic_hook_info| {
-        let panic_location = panic_hook_info.location().map(PanicLocation::from);
-        // TODO: this will happen as soon as we try to do shrinking. I don't
-        // think it's as simple as just making it compile though - the panic
-        // handler being a global resource is really kinda screwing us here, and
-        // I think trying to overwrite it at all is a bad idea.
-        tx.try_send(panic_location).unwrap();
-    }));
+        // NOTE(ichen): these thread-locals allow us to smuggle out the panic
+        // location and message from the panic hook and catch_unwind closure,
+        // respectively, without concurrent threads potentially clobbering each
+        // other's data.
+        static PANIC_LOCATION: RefCell<Option<PanicLocation>> = RefCell::new(None);
+        static PANIC_MESSAGE: RefCell<Option<String>> = RefCell::new(None);
+    }
 
+    /// Whether or not the panic hook is currently replaced, and if so how many
+    /// threads are currently relying on the adversary panic hook as well as the
+    /// old panic hook to delegate to in case any unrelated threads panic while
+    /// we've hijacked the panic hook.
+    ///
+    /// This is necessary since the panic hook is a shared resource across all
+    /// threads, and we want to handle potentially multiple concurrent adversary
+    /// panic based tests gracefully.
+    static PANIC_HOOK_STATUS: LazyLock<Arc<Mutex<PanicHookStatus>>> =
+        LazyLock::new(|| Arc::new(Mutex::new(PanicHookStatus::NotReplaced)));
+
+    // This thread is now in an adversary panic based test - if our custom panic
+    // hook detects a panic from this thread, it should have our custom behavior
+    THREAD_PANIC_TESTING.set(true);
+
+    // Replace the panic hook with our custom one. If we've already replaced it,
+    // increment the number of threads currently doing panic based testing.
+    let mut panic_hook_status = PANIC_HOOK_STATUS.lock().unwrap();
+    match *panic_hook_status {
+        PanicHookStatus::NotReplaced => {
+            let old_hook = std::panic::take_hook();
+            *panic_hook_status = PanicHookStatus::Replaced {
+                old_hook,
+                active_testing_threads: 1,
+            };
+
+            std::panic::set_hook(Box::new(|panic_hook_info| {
+                if THREAD_PANIC_TESTING.get() {
+                    let panic_location = panic_hook_info.location().map(PanicLocation::from);
+
+                    PANIC_LOCATION.set(panic_location);
+                } else {
+                    // We're currently running the replaced panic hook, so if
+                    // PANIC_HOOK_STATUS isn't `Replaced`, that's a logic error
+                    let PanicHookStatus::Replaced { old_hook, .. } =
+                        &*PANIC_HOOK_STATUS.lock().unwrap()
+                    else {
+                        unreachable!()
+                    };
+
+                    // This panic wasn't from a thread doing adversary panic
+                    // based testing - delegate to the old panic hook
+                    old_hook(panic_hook_info);
+                }
+            }));
+        }
+        PanicHookStatus::Replaced {
+            ref mut active_testing_threads,
+            ..
+        } => {
+            *active_testing_threads = active_testing_threads.checked_add(1).unwrap();
+        }
+    }
+    drop(panic_hook_status);
+
+    // Actually run the test, letting `run_test` do the bulk of the work for us
     let test_result = run_test(
         |value| {
             std::panic::catch_unwind(|| test(value))
-                .map_err(|e| panic_message.set(extract_panic_message(e)).unwrap())
+                .map_err(|e| PANIC_MESSAGE.set(extract_panic_message(e)))
                 .is_ok()
         },
         generator,
         rng,
-    )
-    .map_err(|report| Report {
+    );
+
+    // This thread is no longer in an adversary panic based test - if our custom
+    // panic hook detects a panic from this thread, it should delegate to the
+    // old panic hook.
+    THREAD_PANIC_TESTING.set(false);
+
+    // Decrement the number of threads currently doing panic based testing,
+    // restoring the original panic hook if we were the last one.
+    let mut panic_hook_status = PANIC_HOOK_STATUS.lock().unwrap();
+    match &mut *panic_hook_status {
+        PanicHookStatus::NotReplaced => unreachable!(),
+        PanicHookStatus::Replaced {
+            active_testing_threads: 0,
+            ..
+        } => {
+            let PanicHookStatus::Replaced {
+                active_testing_threads: 0,
+                old_hook,
+            } = std::mem::replace(&mut *panic_hook_status, PanicHookStatus::NotReplaced)
+            else {
+                // The pattern in this `if let` will match anything that matches
+                // the pattern for the match arm we're in, so entering this else
+                // branch would indicate a logic error.
+                unreachable!()
+            };
+            std::panic::set_hook(old_hook);
+        }
+        PanicHookStatus::Replaced {
+            active_testing_threads: active_testing_threads @ 1..,
+            ..
+        } => {
+            *active_testing_threads = active_testing_threads.checked_sub(1).unwrap();
+        }
+    }
+    drop(panic_hook_status);
+
+    // Extract out the panic message and location from our thread-locals, and
+    // insert them into the report.
+    let test_result = test_result.map_err(|report| Report {
         panic_info: Some(PanicInfo {
-            message: panic_message.into_inner().flatten(),
-            location: rx.try_recv().ok().flatten(),
+            message: PANIC_MESSAGE.take(),
+            location: PANIC_LOCATION.take(),
         }),
         ..report
     });
-
-    std::panic::set_hook(old_panic_hook);
 
     test_result
 }
