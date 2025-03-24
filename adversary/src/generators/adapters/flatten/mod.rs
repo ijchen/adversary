ab808bd025371f218e8eef6a81b48102ac34cd7c
root <root@isaacs-windows>
2025-03-23 23:11:16 -0700

    added TODO comment

--- adversary/src/generators/adapters/flatten/mod.rs
+++ adversary/src/generators/adapters/flatten/mod.rs
@@ -12,5 +12,6 @@ mod shrinker_owner;
 pub fn flatten<P: IntoValueGen<C>, C: IntoValueGen<T>, T>(
     parent_gen: P,
 ) -> impl ValueGen<Value = T> {
+    // TODO: call .into_value_gen(..) here instead of in Flatten::new(..)
     Flatten::new(parent_gen)
 }
