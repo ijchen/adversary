7a2f8636aacddc2a374ac647b90db9ed9eb2ffac
Isaac Chen <isaacjchen1@gmail.com>
2025-09-04 11:40:40 -0700

    added todo comment for shrinker idea

--- adversary/src/shrinker/mod.rs
+++ adversary/src/shrinker/mod.rs
@@ -4,5 +4,9 @@ mod shrinker;
 // TODO(ijchen): I think I want to publicly expose this module, probably as
 // `shrinking` or something.
 
+// TODO: generic shrinker that wraps another shrinker but just adds a step at
+// the end that runs the final failing inputs many times to determine if it
+// fails reliably, or if some percentage of the time it actually passes
+
 pub use never_shrink::NeverShrink;
 pub use shrinker::Shrinker;
