152afc5959deec66a95eb2b38e6dbc80c2652950
Isaac Chen <isaacjchen1@gmail.com>
2025-09-22 21:08:33 -0400

    updated documentation for RangeAwareValueGen

--- adversary/src/value_gen/range_aware_value_gen.rs
+++ adversary/src/value_gen/range_aware_value_gen.rs
@@ -1,7 +1,11 @@
 use super::ValueGen;
 
 pub trait RangeAwareValueGen: ValueGen {
-    /// Returns whether or not a given value is in the range of possible values
-    /// produced by this [`ValueGen`].
+    /// Returns whether or not a given value is in the range of possible values produced by this
+    /// [`ValueGen`].
+    ///
+    /// This function should always be deterministic and correct - it is a logic error to implement
+    /// this trait for a [`ValueGen`] that cannot reliably determine whether or not any given value
+    /// is in its range.
     fn value_in_range(&self, value: &Self::Value) -> bool;
 }
